70034c9ef30743254a1432f11041f93f99460662
Alan Vieira Martins <alanhorcrux@gmail.com>
2021-11-26 10:01:38 -0300

    Create chat with Pattern Mediator

---
+++ DesignPatternMediator/DesignPatternMediator/ChatApp/Developer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternMediator.ChatApp
+{
+    public class Developer : TeamMember
+    {
+        public Developer(string name) : base(name)
+        {
+        }
+
+        public override void Receive(string from, string message)
+        {
+            Console.WriteLine($"{this.Name} ({nameof(Developer)}) has received");
+            base.Receive(from, message);
+        }
+    }
+}
--- DesignPatternMediator/DesignPatternMediator/Program.cs
+++ DesignPatternMediator/DesignPatternMediator/Program.cs
@@ -1,4 +1,5 @@
-﻿using DesignPatternMediator.Structural;
+﻿using DesignPatternMediator.ChatApp;
+using DesignPatternMediator.Structural;
 using System;
 
 namespace DesignPatternMediator
@@ -6,6 +7,23 @@ namespace DesignPatternMediator
     public class Program
     {
         static void Main(string[] args)
+        {
+            var teamChat = new TeamChatroom();
+
+            var steve = new Developer("Steve");
+            var justin = new Developer("Justin");
+            var jenna = new Developer("Jenna");
+            var kim = new Tester("Kim");
+            var julia = new Tester("Julia");
+            teamChat.RegisterMembers(steve, justin, jenna, kim, julia);
+
+            steve.Send("Hey everyone, we're going to be deploying at 2pm today.");
+            kim.Send("Ok, thanks for letting us know");
+
+            steve.SendTo<Developer>("We have a problem!");
+        }
+
+        private static void Structural()
         {
             //Execute Structura = new StructuralService();
             //Structura.Run();
