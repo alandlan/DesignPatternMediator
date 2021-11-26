3b49e76b87219f75ad1eccd6e82f63d442f63c1d
Alan Vieira Martins <alanhorcrux@gmail.com>
2021-11-26 09:28:39 -0300

    Ajuste

--- DesignPatternMediator/DesignPatternMediator/Program.cs
+++ DesignPatternMediator/DesignPatternMediator/Program.cs
@@ -3,24 +3,15 @@ using System;
 
 namespace DesignPatternMediator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            var mediator = new ConcreteMediator();
-            //var c1 = new Colleague1();
-            //var c2 = new Colleague2();
-            ////mediator.Colleague1 = c1;
-            ////mediator.Colleague2 = c2;
-            //mediator.Register(c1);
-            //mediator.Register(c2);
+            //Execute Structura = new StructuralService();
+            //Structura.Run();
 
-            var c1 = mediator.CreateColleague<Colleague1>();
-            var c2 = mediator.CreateColleague<Colleague2>();
-
-            c1.Send("Hello, (from c1)");
-            c2.Send("Hei, (from c2)");
-
+            //IProgram<StructuralService> _Structural = new StructuralService();
+            //_Structural.Run();
         }
     }
 }
---
+++ DesignPatternMediator/DesignPatternMediator/Structural/StructuralService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternMediator.Structural
+{
+    public class StructuralService : Execute, IProgram<StructuralService>
+    {
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override void Run()
+        {
+            var mediator = new ConcreteMediator();
+            //var c1 = new Colleague1();
+            //var c2 = new Colleague2();
+            ////mediator.Colleague1 = c1;
+            ////mediator.Colleague2 = c2;
+            //mediator.Register(c1);
+            //mediator.Register(c2);
+
+            var c1 = mediator.CreateColleague<Colleague1>();
+            var c2 = mediator.CreateColleague<Colleague2>();
+
+            c1.Send("Hello, (from c1)");
+            c2.Send("Hei, (from c2)");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
